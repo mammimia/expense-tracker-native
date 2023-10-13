ea00b1b8f0a54d6a05f1211a04cef1067d583a84
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-13 10:27:04 +0300

    🎨 style(App.js): add custom styles for header, tab bar, and tab icons to improve visual consistency 📦 feat(styles.js): add global styles file to store commonly used colors for the app

--- App.js
+++ App.js
@@ -5,24 +5,44 @@ import { StatusBar } from 'expo-status-bar';
 import AllExpenses from './screens/AllExpenses';
 import ManageExpenses from './screens/ManageExpenses';
 import RecentExpenses from './screens/RecentExpenses';
+import { GlobalStyles } from './constants/styles';
+import { Ionicons } from '@expo/vector-icons';
 
 const Stack = createStackNavigator();
 const Bottom = createBottomTabNavigator();
 
 const ExpensesOverview = () => (
-  <Bottom.Navigator>
+  <Bottom.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: GlobalStyles.colors.primary500
+      },
+      headerTintColor: 'white',
+      tabBarStyle: {
+        backgroundColor: GlobalStyles.colors.primary500
+      }
+    }}
+  >
     <Bottom.Screen
       name="recent-expenses"
       component={RecentExpenses}
       options={{
-        title: 'Recent Expenses'
+        title: 'Recent Expenses',
+        tabBarLabel: 'Recent',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="hourglass" color={color} size={size} />
+        )
       }}
     />
     <Bottom.Screen
       name="all-expenses"
       component={AllExpenses}
       options={{
-        title: 'All Expenses'
+        title: 'All Expenses',
+        tabBarLabel: 'All',
+        tabBarIcon: ({ color, size }) => (
+          <Ionicons name="calendar" color={color} size={size} />
+        )
       }}
     />
   </Bottom.Navigator>
