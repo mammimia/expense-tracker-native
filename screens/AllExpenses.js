486af0f904908e32535b49304b5a8cc25e62d4a8
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-13 08:58:10 +0300

    🔧 chore(.gitignore): add .prettierrc to the list of ignored files to exclude Prettier configuration file from version control
    📦 deps(package.json): add dependencies for react-navigation and related packages to enable navigation in the app
    
    📄 add(screens/AllExpenses.js): create a new screen component for displaying all expenses in the app
    
    📄 add(screens/ManageExpenses.js): create a new screen component for managing expenses in the app
    
    📄 add(screens/RecentExpenses.js): create a new screen component for displaying recent expenses in the app

---
+++ screens/AllExpenses.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function AllExpenses() {
+  return (
+    <View>
+      <Text>AllExpenses</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default AllExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+function RecentExpenses() {
+  return (
+    <View>
+      <Text>RecentExpenses</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default RecentExpenses;
