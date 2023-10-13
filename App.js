8d0bd48a8deed9c6ff5453f3780cae32080a4d23
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-13 09:09:24 +0300

    🔧 chore(App.js): refactor App.js to use react-navigation library for navigation 🔧 chore(ManageExpenses.js): remove unnecessary newline at end of file 🔧 chore(RecentExpenses.js): remove unnecessary newline at end of file

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import AllExpenses from './screens/AllExpenses';
+import ManageExpenses from './screens/ManageExpenses';
+import RecentExpenses from './screens/RecentExpenses';
+
+const Stack = createStackNavigator();
+const Bottom = createBottomTabNavigator();
+
+const ExpensesOverview = () => (
+  <Bottom.Navigator>
+    <Bottom.Screen
+      name="recent-expenses"
+      component={RecentExpenses}
+      options={{
+        title: 'Recent Expenses'
+      }}
+    />
+    <Bottom.Screen
+      name="all-expenses"
+      component={AllExpenses}
+      options={{
+        title: 'All Expenses'
+      }}
+    />
+  </Bottom.Navigator>
+);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="expenses-overview"
+            component={ExpensesOverview}
+            options={{
+              headerShown: false
+            }}
+          />
+          <Stack.Screen
+            name="manage-expenses"
+            component={ManageExpenses}
+            options={{
+              title: 'Manage Expenses'
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
