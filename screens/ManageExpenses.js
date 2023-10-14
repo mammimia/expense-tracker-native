69b0280aa70ec810f6a0f351e101dcdd641e8c01
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-14 10:14:52 +0300

    🔨 refactor(ManageExpenses.js): reorganize imports and add missing imports for IconButton and Button components 🚀 feat(ManageExpenses.js): implement deleteHandler, saveHandler, and cancelHandler functions for managing expenses 🎨 style(ManageExpenses.js): add styles for rootContainer, deleteContainer, buttonContainer, and button components to improve layout and appearance

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import IconButton from '../components/ui/IconButton';
+import { GlobalStyles } from '../constants/styles';
+import Button from '../components/ui/Button';
 
 function ManageExpenses({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -11,13 +14,58 @@ function ManageExpenses({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
+  function deleteHandler() {}
+
+  function saveHandler() {}
+
+  function cancelHandler() {}
+
   return (
-    <View>
-      <Text>ManageExpenses</Text>
+    <View style={styles.rootContainer}>
+      <View style={styles.buttonContainer}>
+        <Button style={styles.button} onPress={cancelHandler} mode="flat">
+          Cancel
+        </Button>
+        <Button style={styles.button} onPress={saveHandler}>
+          Save
+        </Button>
+      </View>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            size={24}
+            color={GlobalStyles.colors.error500}
+            onPress={deleteHandler}
+          />
+        </View>
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary100
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8
+  }
+});
 
 export default ManageExpenses;
