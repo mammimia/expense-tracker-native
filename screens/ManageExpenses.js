3b658b924d104288c00de059efa33bda47b2c826
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2023-10-14 09:57:46 +0300

    🔀 refactor(ManageExpenses.js): refactor ManageExpenses component to use useLayoutEffect hook for setting navigation options 🐛 fix(ManageExpenses.js): fix missing dependency warning by adding navigation and isEditing to the dependency array of useLayoutEffect hook

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -1,6 +1,16 @@
+import { useLayoutEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-function ManageExpenses() {
+function ManageExpenses({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? 'Edit Expense' : 'Add Expense'
+    });
+  }, [navigation, isEditing]);
+
   return (
     <View>
       <Text>ManageExpenses</Text>
